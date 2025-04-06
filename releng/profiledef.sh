00b6130295c33b1925397830b4c954726af7df83
SotoEstevez <ricardo@sotoestevez.dev>
2025-04-06 22:08:19 +0200

    edit: profiledef

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="Archrio"
+iso_label="ARCHRIO_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="SotoEstevez <https://www.sotoestevez.dev>"
+iso_application="Archrio Linux Live ISO"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
