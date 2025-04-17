b985c3fc26b146ba74544e9de9b34960c24ed9a3
SotoEstevez <ricardo@sotoestevez.dev>
2025-04-17 15:26:22 +0200

    feat: create user in passwd

--- build_iso.fish
+++ build_iso.fish
@@ -1,6 +1,14 @@
 #!/usr/bin/fish
 
-set -q MIRROR_LIST; or set -f MIRROR_LIST releng/archrio.mirrorlist
+set -f iso_folder releng
+
+if contains -- --generate-user $argv
+    set -f etc $iso_folder/airootfs/etc
+    read -l -P "Username: " -l username
+    echo "$username:x:1000:1000::/home/$username:/usr/bin/fish" >>$etc/passwd
+end
+
+set -q MIRROR_LIST; or set -f MIRROR_LIST $iso_folder/archrio.mirrorlist
 if contains -- --update-mirrors $argv
     echo "Updating mirrors..."
     set -q MIRRORS_COUNTRY || set -l MIRRORS_COUNTRY ES
@@ -20,7 +28,7 @@ set -q OUT_FOLDER || set -f OUT_FOLDER (pwd)/out
 echo "Updating pacman repos"
 pacman -Syy
 echo "Starting mkarchiso script"
-mkarchiso -v -w $WORK_FOLDER -o $OUT_FOLDER releng
+mkarchiso -v -w $WORK_FOLDER -o $OUT_FOLDER $iso_folder
 
 set -l new_iso (eza --absolute --no-symlinks -r --sort date $OUT_FOLDER | head -1 )
 set -l last_iso $OUT_FOLDER/Archrio-LATEST.iso
@@ -29,3 +37,8 @@ ln -sf $new_iso $last_iso
 
 echo "Tearing up script artifacts"
 rm -rf $WORK_FOLDER
+
+if contains -- --generate-user $argv
+    echo "Restoring user files"
+    cat $etc/passwd | head -n -1 | sponge $etc/passwd
+end
