2f77ff43166ea4f9a968cf7c3d9830d2354d9068
SotoEstevez <ricardo@sotoestevez.dev>
2025-06-16 17:28:51 +0200

    feat: install permissions

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -20,6 +20,7 @@ file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
+  ["/root/install.sh"]="0:0:755"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
